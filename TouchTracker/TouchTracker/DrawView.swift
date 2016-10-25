4d005844a0f9d84860a1c22fa0ff377ed548a322
Jeffrey Becker <jeffrey.becker@eagles.oc.edu>
2016-10-24 20:59:26 -0500

    Creating initial project structure

---
+++ TouchTracker/TouchTracker/DrawView.swift
@@ -0,0 +1,14 @@
+//
+//  DrawView.swift
+//  TouchTracker
+//
+//  Created by Jeffrey Becker on 10/24/16.
+//  Copyright © 2016 Jeffrey Becker. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    var currentLine: Line?
+    var finishedLines = [Line]()
+}
---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Jeffrey Becker on 10/24/16.
+//  Copyright © 2016 Jeffrey Becker. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
